4a89fac4f8d17c204eb466ff776ef2212d7836ab
ufolux <richadlu001@gmail.com>
2025-10-20 22:54:10 -0400

    deploy: add github action yaml

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-        tailwindcss(),
-        svelte()
-  ],
-})
+  base: "/richhood/",
+  plugins: [tailwindcss(), svelte()],
+});
